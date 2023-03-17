91fcff115626d57dbfc4272b1cbfd44952f62de2
HyperionThe1st <101032923+HyperionThe1st@users.noreply.github.com>
2023-03-17 09:16:52 +0700

    BotCollectBrick

--- Assets/_Game/Scripts/Player/Bot.cs
+++ Assets/_Game/Scripts/Player/Bot.cs
@@ -12,11 +12,12 @@ public class Bot : Character
     }
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Hit1");
         switch (other.tag)
         {
             case Variable.NOCOLORBRICK:
                 {
-                    Debug.Log("Hit");
+                    Debug.Log("Hit2");
                     Brick brick = other.GetComponent<Brick>();
                     if (brick != null)
                     {
@@ -30,6 +31,28 @@ public class Bot : Character
                     break;
                 }
         } 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit3");
+        switch (collision.gameObject.tag)
+        {
+            case Variable.NOCOLORBRICK:
+                {
+                    
+                    Brick brick = collision.gameObject.GetComponent<Brick>();
+                    if (brick != null)
+                    {
+                        BrickColor numbTemp = brick._brickColor;
+                        if (player_number == numbTemp)
+                        {
+                            AddBrick(_char, brick);
+                            int count = _char.brick_count;
+                        }
+                    }
+                    break;
+                }
+        }
     }
 
 }
